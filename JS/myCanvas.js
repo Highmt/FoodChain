3143c4be784e4b8dfd7a95ca7e2aa0b4cac53471
Yuta Takayama <yprogdbcs@icloud.com>
2020-06-28 23:04:07 +0900

    ライオンとシマウマの死亡を追加

--- JS/myCanvas.js
+++ JS/myCanvas.js
@@ -6,11 +6,13 @@ if(canvas.getContext){
     canvas.height = document.documentElement.clientHeight-10;
     var grass_density = 10;
 
-    var zebras = new Array(100).fill(0);
+    var zebras = new Array(50).fill(0);
     zebras = zebras.map(item => new Zebra());
 
     var lions = new Array(10).fill(0);
     lions = lions.map(item => new Lion());
+
+    var animals = zebras.concat(lions);
 
     var grass = new Array(Math.floor(canvas.width / grass_density)).fill(0);
     for(let i = 0; i < canvas.width / grass_density; i++) {
@@ -24,8 +26,9 @@ if(canvas.getContext){
 }
 
 function draw() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.clearRect(0, 0, canvas.width, canvas.height); // canvasをリセット
 
+    //　草を描画
     ctx.fillStyle="rgb(0,225,0)";//塗りつぶしの色
     grass.forEach(items => {
         items.forEach(item => {
@@ -35,17 +38,17 @@ function draw() {
         });
     });
 
-    zebras.forEach(item => {
+
+    animals.forEach((item, i) => {
+        if (item.status === 1) {
+            item.body_count++;
+        }
         item.draw();
         if(!item.eat() && item.status === 0){
             item.move();
         }
-    });
-
-    lions.forEach(item => {
-        item.draw();
-        if(!item.eat() && item.status === 0){
-            item.move();
+        if(item.body_count > 100){
+            animals.splice(i, 1);  //配列からの取り除き方用検討
         }
     });
 }
